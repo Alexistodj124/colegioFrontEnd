9326cc0ee42c87aeb27eb34b94f267aecec6bbdf
Alexistodj124 <55223401+Alexistodj124@users.noreply.github.com>
2026-02-07 23:58:48 -0600

    api

--- src/api.js
+++ src/api.js
@@ -1,4 +1,4 @@
-const API_BASE = import.meta.env.VITE_API_BASE || "http://127.0.0.1:8000";
+const API_BASE = import.meta.env.VITE_API_BASE || "http://3.140.209.59/api5";
 
 export function getToken() {
   return localStorage.getItem("token") || "";
